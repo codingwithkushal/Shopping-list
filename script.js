36546d01c3f28e9bb8baa918666e0aae28edb9b7
codingwithkushal <mekushaljoshi@gmail.com>
2024-12-26 15:47:55 +0530

    Your commit message

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 function addItem(e) {
     e.preventDefault();
@@ -23,6 +25,7 @@ function addItem(e) {
     itemList.appendChild(li);
   
     itemInput.value = '';
+    checkUI();
   }
   
   function createButton(classes) {
@@ -38,4 +41,50 @@ function addItem(e) {
     icon.className = classes;
     return icon;
   }
-    
+  function removeItem(e) {
+    if (e.target.parentElement.classList.contains('remove-item')) {
+      if (confirm('Are you sure?')) {
+        e.target.parentElement.parentElement.remove();
+      }
+    }
+    checkUI();
+  }
+  function clearItems() {
+    if (confirm('Are you sure?')) {
+      while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+      }
+    }
+    checkUI();
+  }
+  function checkUI() {
+    const items = itemList.querySelectorAll('li');
+  
+    if (items.length === 0) {
+      clearBtn.style.display = 'none';
+      itemFilter.style.display = 'none';
+    } else {
+      clearBtn.style.display = 'block';
+      itemFilter.style.display = 'block';
+    }
+  }
+  function filterItems(e) {
+    const items = itemList.querySelectorAll('li');
+    const text = e.target.value.toLowerCase();
+  
+    items.forEach((item) => {
+      const itemName = item.firstChild.textContent.toLowerCase();
+  
+      if (itemName.indexOf(text) != -1) {
+        item.style.display = 'flex';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
+  itemForm.addEventListener('submit', addItem);
+  itemList.addEventListener('click', removeItem);
+  clearBtn.addEventListener('click', clearItems);
+  itemFilter.addEventListener('input', filterItems);
+
+  checkUI();
